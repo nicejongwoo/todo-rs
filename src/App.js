6c33f8755191c1665d56bdacc60706d6acc2f97a
jongwoo <leejongwoo.dev@gmail.com>
2021-10-18 21:51:22 +0900

    material ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import Todo from './Todo';
 import './App.css';
 import React from 'react';
+import { Paper, List } from "@material-ui/core";
 
 
 class App extends React.Component {
 
     constructor(props){
         super(props);
-        // this.state = {
-        //     item: { id: 1, title: "hello react", done: true }
-        // };
         this.state = {
             items: [
                 { id: 1, title: "hello react1", done: true },
@@ -20,9 +18,15 @@ class App extends React.Component {
 
     
     render() {
-        var todoItems = this.state.items.map((item, index) => (
-            <Todo item={item} key={item.id} />
-        ));
+        var todoItems = this.state.items.length > 0 && (
+            <Paper stype={{margin: 16}}>
+                <List>
+                    {this.state.items.map((item, idx)=> (
+                        <Todo item={item} key={item.id}/>
+                    ))}
+                </List>
+            </Paper>
+        );
 
         return <div className="App">{todoItems}</div>;
         
