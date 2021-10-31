e0fbc633b089265226160fea99bd75ea8e111ac9
jongwoo <leejongwoo.dev@gmail.com>
2021-10-31 19:01:02 +0900

    localStorage에 토큰 저장, 요청 헤더에 토큰 정보 추가

--- ui/src/app/service/ApiService.js
+++ ui/src/app/service/ApiService.js
@@ -1,10 +1,18 @@
 import { API_BASE_URL } from "../config/app-config";
 
+const ACCESS_TOKEN = "ACCESS_TOKEN";
+
 export function call(api, method, request) {
+    let headers = new Headers({ "Content-Type": "application/json" });
+
+    //로컬스토리지에서 ACCESS_TOKEN 가져오기
+    const accessToken = localStorage.getItem(ACCESS_TOKEN);
+    if(accessToken && accessToken != null) {
+        headers.append("Authorization", "Bearer " + accessToken);
+    }
+
     let options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
-        }),
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
@@ -21,13 +29,22 @@ export function call(api, method, request) {
             }
             return json;
         })
-    ); 
+    )
+    .catch((error) => {
+        if(error.status === 403) {
+            window.location.href = "/login";
+        }
+        return Promise.reject(error);
+    });
 }
 
 export function signin(userDTO) {
     return call("/auth/signin", "POST", userDTO) 
         .then((response) => {
             if(response.token) {
+                //로컬스토리지에 토큰저장
+                localStorage.setItem("ACCESS_TOKEN", response.token);
+                //token이 존재할 경우 todo 목록 화면으로 이동
                 window.location.href = "/";
             }
         });
