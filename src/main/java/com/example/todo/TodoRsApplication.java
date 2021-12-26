9e2a77483dbcdde2c3f02ef6d29296c80f1855b5
jongwoo <leejongwoo.dev@gmail.com>
2021-12-26 11:01:20 +0900

    war 패키징 수정, readme 수정

--- src/main/java/com/example/todo/TodoRsApplication.java
+++ src/main/java/com/example/todo/TodoRsApplication.java
@@ -2,12 +2,18 @@ package com.example.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TodoRsApplication {
+public class TodoRsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoRsApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TodoRsApplication.class);
+	}
 }
